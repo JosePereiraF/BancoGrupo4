bdeb1e881137a45fc9b10a3817bb752651ac9b3b
Samuel Romeu <samuelromeu20063@gmail.com>
2024-04-12 14:58:32 -0300

    Comecei a criar o método criaContaCorrente, ainda está incompleto

--- banco/src/project/entities/conta/ContaCorrente.java
+++ banco/src/project/entities/conta/ContaCorrente.java
@@ -1,11 +1,14 @@
 package project.entities.conta;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Random;
 
 import project.enums.TipoContaENUM;
 
 public class ContaCorrente extends Conta{
 
+	public HashMap<String,ContaCorrente>  lista_contaCorrente = new HashMap();
 	
 	public ContaCorrente(String cpf, int numeroDaConta, TipoContaENUM tipoConta, double saldo, LocalDate dataCriacao) {
 		super(cpf, numeroDaConta, tipoConta, saldo, dataCriacao);
@@ -15,6 +18,33 @@ public class ContaCorrente extends Conta{
 	public String toString() {
 		return "ContaCorrente [cpf=" + cpf + ", numeroDaConta=" + numeroDaConta + ", tipoConta=" + tipoConta
 				+ ", saldo=" + saldo + ", dataCriacao=" + dataCriacao + "]";
+	}
+	
+	
+	
+	public void criarContaCorrente(String key) {
+		//cpf vai ser a key
+		//numeroDaConta -aleatorizar
+		//Tipo conta sempre TipoContaENUM.CONTACORRENTE
+		//saldo -sempre iniciar em 0
+		//diaCriacao vai sempre no instante (LocalDate.now)
+		
+		/*Sugestões: Alterar o numeroDaConta para String ao invés de Int
+		* diaCriacao ser LocalDate ser LocalDateTime
+		* Segundo o texto do projeto a conta deve ter um atributo identificador da agência.
+		acho que seria ninteressante apenas criarmos 
+		* Seria viável colocar o HashMap na classe Conta?
+		*/ 
+		
+		Random random = new Random();
+		int numeroDaContaGerado = random.nextInt(20);
+		//É necessário cirar um método para ver se numeroDaContaGerado 
+		// já existe e se pertence a outra conta um Numero da conta igual
+		LocalDate diaCriacao = LocalDate.now();
+		
+		
+		ContaCorrente contaCorrente = new ContaCorrente(key,numeroDaContaGerado,TipoContaENUM.CONTACORRENTE,0.0,diaCriacao);
+		lista_contaCorrente.put(key, contaCorrente);
 	}
 
 	
