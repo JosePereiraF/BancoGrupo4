3e433f5130eba65ad15ee066e7ed33be6094a080
jordanaaraujo <jordanaa_araujo@hotmail.com>
2024-04-14 16:06:22 -0300

    Add método criar cliente e buscar cliente.

--- banco/src/project/entities/cliente/Cliente.java
+++ banco/src/project/entities/cliente/Cliente.java
@@ -1,6 +1,7 @@
 package project.entities.cliente;
 
-import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Scanner;
 
 import project.entities.Pessoa;
 
@@ -10,8 +11,10 @@ public class Cliente extends Pessoa{
     //criar metodo para criar um cliente e para busca-lo
 	//adcionar os clientes criados dentro do hashmap
 	
-	public Cliente(String nome, String cpf, String senha, LocalDate dataNascimento, String numeroAgencia) {
-		super(nome, cpf, senha, dataNascimento);
+	Scanner sc = new Scanner(System.in);
+	HashMap<String, Cliente>lista_cliente = new HashMap<>();
+	public Cliente(String nome, String cpf, String senha,String numeroAgencia) {
+		super(nome, cpf, senha );
 		this.numeroAgencia = numeroAgencia;
 	}
 
@@ -21,7 +24,32 @@ public class Cliente extends Pessoa{
 				+ ", dataNascimento=" + dataNascimento + "]";
 	}
 
+	public void criar_cliente() {
+		//(String nome, String cpf, String senha, LocalDate dataNascimento, String numeroAgencia) 
+		String nomeDigitado;
+		String cpfDigitado;
+		String senhaDigitada;
+		String numeroAgenciaDigitada;
+		
+		System.out.println("Digite o nome: ");
+		nomeDigitado = sc.nextLine();
+		System.out.println("Digite o CPF: ");
+		cpfDigitado = sc.nextLine();
+		System.out.println("Digite a senha: ");
+		senhaDigitada = sc.nextLine();
+		System.out.println("Digite o número da agência: ");
+		numeroAgenciaDigitada = sc.nextLine();
+		
+		Cliente clinte = new Cliente(nomeDigitado,cpfDigitado,senhaDigitada,numeroAgenciaDigitada);
+		lista_cliente.put(cpfDigitado, clinte);
+		
+	}
 	
+		public Cliente buscar_cliente(String key) {
+			
+			return lista_cliente.get(key);
+			
+		}
 	
 	
 }
