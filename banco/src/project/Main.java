1549f45f996125c01488071d5ab3f18763f9b66c
JosePereiraF <jose222125santos@gmail.com>
2024-04-16 21:11:45 -0300

    limpeza no main

--- banco/src/project/Main.java
+++ banco/src/project/Main.java
@@ -10,16 +10,7 @@ import project.funcionalidades.InOutUtils;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-	//criar clientes e contas 
-		Cliente cliente = new Cliente();
-		Conta conta = new ContaCorrente();
-		
-		InOutUtils.leitor_pessoa();
-		InOutUtils.leitor_contas();
-		
-		
-//		Conta.criarConta("15798");
-		System.out.println(Conta.listaConta.get("15798"));
+	
 		
 
 
